e30309fdd29046a8203795a14543e28e0fba722e
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2018-12-17 10:05:20 -0600

    Update curses_keys.py

--- curses_keys.py
+++ curses_keys.py
@@ -9,7 +9,7 @@ x = 10.0; y = 10.0
 screen = curses.initscr(); curses.noecho(); curses.halfdelay(3); screen.keypad(True); screen = curses.initscr() #set up curses file
 curses.start_color(); curses.use_default_colors()
 curses.init_pair(1, curses.COLOR_RED, -1); curses.init_pair(2, curses.COLOR_GREEN, -1); curses.init_pair(3, curses.COLOR_BLUE, -1)
-key = ''
+speed = 1; key = ''
 def test(x, y): #function to test if the arm is in the range of possible motion
     d_three = (round(y, 1) ** 2 + round(x, 1) ** 2) ** 0.5
     if d_three > d_one + d_two or d_three < d_one - d_two or (math.fabs(round(x, 1)) == 0.0 and math.fabs(round(y, 1)) == 0.0):
@@ -32,38 +32,47 @@ while key != ord('q'): #to end loop if 'q' is hit
     input_shoulder = int(fraction_shoulder * a_shoulder * 2000 / math.pi + 400) #angle and motor value calculations
     RPL.servoWrite(s_pin, input_shoulder)
     RPL.servoWrite(e_pin, input_elbow) #to move the motors
-    screen.addstr(0, 0, 'Hit   to quit. Use the            to move the arm.'); screen.addstr(0, 4, 'Q', curses.color_pair(1)); screen.addstr(0, 23, 'arrow keys', curses.color_pair(2)); screen.addstr(0, 51, 'Detected key:') #print code controls
+    screen.addstr(0, 0, 'Hit   to quit. Use the            to move and "X" and "Z" for speed:'); screen.addstr(0, 4, 'Q', curses.color_pair(1)); screen.addstr(0, 23, 'arrow keys', curses.color_pair(2)) #print code controls
     screen.addstr(1, 0, 'Shoulder angle:'); screen.addstr(1, 22, 'Elbow angle:')
     screen.addstr(2, 0, 'Point:'); screen.addstr(3, 0, '(    ,    )')
     screen.addstr(3, 1, str(round(x, 1)), curses.color_pair(3)); screen.addstr(3, 6, str(round(y, 1)), curses.color_pair(3)) #print point
-    screen.addstr(4, 0, 'Elbow motor value:'); screen.addstr(5, 0, 'Shoulder motor value:')
+    screen.addstr(5, 0, 'Elbow motor value:'); screen.addstr(6, 0, 'Shoulder motor value:')
     screen.addstr(1, 16, str(round(angle_shoulder, 1)), curses.color_pair(2)); screen.addstr(1, 35, str(round(angle_elbow, 1)), curses.color_pair(2)) #print angles
-    screen.addstr(4, 19, str(input_elbow)); screen.addstr(5, 22, str(input_shoulder)) #print all values
-    if (input_shoulder or input_elbow) < 400:
-        screen.addstr(6, 0, 'Point outside of motor domain', curses.color_pair(1))
+    screen.addstr(5, 19, str(input_elbow)); screen.addstr(6, 22, str(input_shoulder)) #print all values
+    screen.addstr(4, 0, 'Speed:'); screen.addstr(4, 7, str(round(speed, 1)))
+    if input_shoulder < 400 or input_elbow < 400:
+        screen.addstr(7, 0, 'Point outside of motor domain', curses.color_pair(1))
     if key != curses.ERR: #to read if the user is pressing a key
         if key == curses.KEY_UP:
-            screen.addstr(0, 65, 'up arrow', curses.color_pair(2))
-            y = y + 0.1; test(x, y)
+            screen.addstr(0, 69, 'up arrow', curses.color_pair(2))
+            y = y + 0.1 * speed; test(x, y)
             if test(x, y) == False:
-                y = y - 0.1
+                y = y - 0.1 * speed
         elif key == curses.KEY_DOWN:
-            screen.addstr(0, 65, 'down arrow', curses.color_pair(2))
-            y = y - 0.1; test(x, y)
+            screen.addstr(0, 69, 'down arrow', curses.color_pair(2))
+            y = y - 0.1 * speed; test(x, y)
             if test(x, y) == False:
-                y = y + 0.1
+                y = y + 0.1 * speed
         elif key == curses.KEY_RIGHT:
-            screen.addstr(0, 65, 'right arrow', curses.color_pair(2))
-            x = x + 0.1; test(x, y)
+            screen.addstr(0, 69, 'right arrow', curses.color_pair(2))
+            x = x + 0.1 * speed; test(x, y)
             if test(x, y) == False:
-                x = x - 0.1
+                x = x - 0.1 * speed
         elif key == curses.KEY_LEFT:
-            screen.addstr(0, 65, 'left arrow', curses.color_pair(2))
-            x = x - 0.1; test(x, y)
+            screen.addstr(0, 69, 'left arrow', curses.color_pair(2))
+            x = x - 0.1 * speed; test(x, y)
             if test(x, y) == False:
-                x = x + 0.1
+                x = x + 0.1 * speed
+        elif key == ord('x'):
+            speed = int(2 * speed)
+            if speed >= 8:
+                speed = 8
+        elif key == ord('z'):
+            speed = int(0.5 * speed)
+            if speed <= 1:
+                speed = 1
         elif key == ord('q'):
             curses.endwin()
         else:
-            screen.addstr(0, 65, 'invalid', curses.color_pair(1))
+            screen.addstr(0, 69, 'invalid', curses.color_pair(1))
             curses.beep()
