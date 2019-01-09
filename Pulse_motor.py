5db84605fa84e7fe9f8a1092fe1628b21caafbac
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2019-01-09 10:11:44 -0600

    Update Pulse_motor.py

--- Pulse_motor.py
+++ Pulse_motor.py
@@ -4,6 +4,8 @@ RPL.RoboPiInit("/dev/ttyAMA0", 115200) #connect to RoboPi
 RPL.pinMode(0, RPL.PWM) #set shoulder_pul pin as a pulse-width modulation output
 RPL.pinMode(1, RPL.OUTPUT) #set shoulder_dir pin to an output and write 1 to it
 
+motor_speed = 1000
+
 def stop(): #stop the motors
   RPL.pwmWrite(0, 0, 1)
 
@@ -15,10 +17,13 @@ def shoulder(dir, run_for, motor_speed):
   RPL.pwmWrite(0, motor_speed, motor_speed * 2) #RPL.pwmWrite(pin value, on duration, off duration)
   time.sleep(run_for)
   stop()
-
-print "Hit 'y' or 'x' to run motor"
-run = raw_input('- ')
-if run == 'y':
-    shoulder(True, 13, 1000)
-if run == 'x':
-    shoulder(False, 13, 1000)
+while True:
+    print "Hit 'y' or 'x' to run motor. Hit 'q' to quit."
+    run = raw_input('- ')
+    if run == 'y':
+        shoulder(True, 13, 1000)
+    elif run == 'x':
+        shoulder(False, 13, 1000)
+    elif run == 'q':
+      break
+    continue
