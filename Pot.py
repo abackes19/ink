a279a25e6e14634f553c2473aa59f4c9dbcc4bd3
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2019-03-06 11:31:26 -0600

    Update Pot.py

--- Pot.py
+++ Pot.py
@@ -7,7 +7,7 @@ ppin = 7  #potentiomenter pin line
 
 while True:
   p1 = RPL.analogRead(ppin)
-  a1 = p1 * 580 / 77
+  a1 = p1 * 145 / 512
   RPL.servoWrite(apin,int(a1))
   print("p = ", int(p1), "  a = ",int(a1 - 55) )
   sleep(1)
