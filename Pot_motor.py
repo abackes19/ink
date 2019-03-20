2e17d2982559e42bdb4443997622ec86f3ff3767
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2019-03-20 10:37:23 -0500

    Update Pot_motor.py

--- Pot_motor.py
+++ Pot_motor.py
@@ -8,17 +8,18 @@ RPL.pinMode(0, RPL.PWM) #set shoulder_pul pin as a pulse-width modulation output
 RPL.pinMode(1, RPL.OUTPUT) #set shoulder_dir pin to an output and write 1 to it
 
 ppin = 7
-p1 = RPL.analogRead(ppin) * 145 / 512 - 55
-
-print("p = ", int(p1))
-sleep(0.1)
+while True:
+    p1 = RPL.analogRead(ppin) * 145 / 512 - 55
+    print("p = ", int(p1))
+    shoulder()
+    sleep(0.1)
 
 def stop(): #stop the motors
     RPL.pwmWrite(0, 0, 1)
 
 def shoulder(): #to run the motors
     motor_speed = 500
-    if abs(round(pot_1, 0) - round(a_shoulder, 0)) > 3: #so there is a margin of error of max 3 degrees
+    if abs(round(pot_1, 0) - round(a_shoulder, 0)) < 5: #so there is a margin of error of max 3 degrees
         if pot_1 > a_shoulder:
             RPL.digitalWrite(1, 1) #turn clockwise
         else:
