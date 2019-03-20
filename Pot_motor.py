0cf5f7b3bcf28dbba3a3cfe5a0b3c01c5b624863
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2019-03-20 10:57:27 -0500

    Update Pot_motor.py

--- Pot_motor.py
+++ Pot_motor.py
@@ -2,21 +2,8 @@ import RoboPiLib_pwm as RPL #to pull all the files needed to run the motors
 from time import sleep
 RPL.RoboPiInit("/dev/ttyAMA0", 115200) #connect to RoboPi
 
-def stop(): #stop the motors
-    RPL.pwmWrite(0, 0, 1)
-
-def shoulder(): #to run the motors
-    motor_speed = 500
-    if abs(round(p1, 0) - round(a_shoulder, 0)) > 5: #so there is a margin of error of max 3 degrees
-        if p1 > a_shoulder:
-            RPL.digitalWrite(1, 1) #turn clockwise
-        else:
-            RPL.digitalWrite(1, 0) #turn counterclockwise
-        RPL.pwmWrite(0, motor_speed, motor_speed * 2) #RPL.pwmWrite(pin value, number of on pulses, total pulses duration)
-    else:
-        stop()
-
 a_shoulder = 200
+motor_speed = 500
 
 RPL.pinMode(0, RPL.PWM) #set shoulder_pul pin as a pulse-width modulation output
 RPL.pinMode(1, RPL.OUTPUT) #set shoulder_dir pin to an output and write 1 to it
@@ -25,5 +12,12 @@ ppin = 7
 while True:
     p1 = RPL.analogRead(ppin) * 145 / 512 - 55
     print("p = ", int(p1))
-    shoulder()
+    if abs(round(p1, 0) - round(a_shoulder, 0)) > 5: #so there is a margin of error of max 3 degrees
+        if p1 > a_shoulder:
+            RPL.digitalWrite(1, 1) #turn clockwise
+        else:
+            RPL.digitalWrite(1, 0) #turn counterclockwise
+        RPL.pwmWrite(0, motor_speed, motor_speed * 2) #RPL.pwmWrite(pin value, number of on pulses, total pulses duration)
+    else:
+        RPL.pwmWrite(0, 0, 1)
     sleep(0.1)
