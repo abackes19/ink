0a7146e52c6a6de7c4017777fa5e58cb704744f0
Amelia Backes <abackes19@bsmschool.org>
2019-03-20 10:03:05 -0500

    whoo boy some new stuff

--- mouse-ik.py
+++ mouse-ik.py
@@ -1,39 +1,56 @@
 import math, fractions
-import pyautogui, sys
+import pyautogui, sys, curses
 
-# 1365 by 767
+# 1366 by 768
 #
+# math syntax: https://docs.python.org/2/library/math.html
 
+screen = curses.initscr(); curses.noecho(); curses.halfdelay(1); screen.keypad(True); curses.start_color(); curses.use_default_colors()
+curses.init_pair(1, curses.COLOR_RED, -1); curses.init_pair(2, curses.COLOR_GREEN, -1); curses.init_pair(3, curses.COLOR_BLUE, -1)
 
+xmbase, ymbase = pyautogui.size()
+xmbase = xmbase / 2
+ymbase = ymbase / 2
 
-height_of_robot = 6.0 #distance from floor to point (0, 0)
 d_one = 150 #distance from shoulder to elbow
 d_two = 150 #distance from elbow to wrist
 
-xmbase = 682
-ymbase = 382
+pyautogui.moveTo(xmbase, ymbase, duration=.1)
 
-x, y = pyautogui.position()
+while True: #to end loop if 'q' is hit
+    key = screen.getch(); screen.clear()
+    xread, yread = pyautogui.position()
+    x = xread - xmbase
+    y = yread - ymbase
+    sqd_one = math.pow(d_one, 2)
+    sqd_two = math.pow(d_two, 2)
 
-def test(x, y): #function to test if the arm is in the range of possible motion
-    d_three = (round(y, 1) ** 2 + round(x, 1) ** 2) ** 0.5
-    if d_three > d_one + d_two or d_three < d_one - d_two or (math.fabs(round(x, 1)) == 0.0 and math.fabs(round(y, 1)) == 0.0):
-        return False
+    d_three = math.sqrt(math.pow(y, 2) + math.pow(x, 2)) # determining distance from shoulder to wrist ^
+    screen.addstr(1, 1, "Press q to quit")
 
-if x <= 0.0: #so the varaibles can't go out of their range
-    x = 0.0
-if y <= -height_of_robot:
-    y = -height_of_robot
-if round(y, 1) == 0.0:
-    y = 0.0
-
-a_elbow = math.acos((d_one ** 2 + d_two ** 2 - round(y, 1) ** 2 - round(x, 1) ** 2) / (2 * d_one * d_two))
-a_two = math.asin((d_two * math.sin(a_elbow) / (round(y, 1) ** 2 + round(x, 1) ** 2) ** 0.5))
-a_four = math.atan2(round(y, 1) , round(x, 1)) #calculate all angle values
-a_shoulder = a_four + a_two
-angle_elbow = a_elbow * 180 / math.pi
-angle_shoulder = a_shoulder * 180 / math.pi
+    if key != curses.ERR: #to read if the user is pressing a key
+        if key == ord('q'):
+            curses.endwin()
+            break
+    elif x == 0 and y == 0:
+        a_shoulder = 90
+        a_elbow = 0
 
 
-print angle_elbow
-print angle_shoulder
+    elif d_three > d_one + d_two:
+        screen.addstr(2, 1, "Too far away; move closer to the origin")
+
+    else:
+        a_three = math.acos((sqd_one + sqd_two - (math.pow(y, 2) + math.pow(x, 2))) / (2 * d_one * d_two))
+        a_two = math.asin((d_two * math.sin(a_three) / d_three)) # angle between shoulder and wrist
+        a_four = math.atan2(y , x) # angle between 0 line and wrist
+
+        a_shoulder = (a_four + a_two) * 180/math.pi
+
+
+         # angle the shoulder joint should be at from the 0 line
+        a_elbow = a_three * 180/math.pi # elbow angle, flush back to shoulder is 0
+
+        screen.addstr(2, 1, "Distance from base: "); screen.addstr(2, 50, str(round(d_three, 1)))
+        screen.addstr(3, 1, "Shoulder angle: "); screen.addstr(3, 50, str(round(a_shoulder, 1)))
+        screen.addstr(4, 1, "Elbow angle: "); screen.addstr(4, 50, str(round(a_elbow, 1)))
