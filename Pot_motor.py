b87c7e172e0aa3d74002afd189bd10b9037e2a18
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2019-03-21 11:44:12 -0500

    Update Pot_motor.py

--- Pot_motor.py
+++ Pot_motor.py
@@ -1,5 +1,4 @@
 import RoboPiLib_pwm as RPL #to pull all the files needed to run the motors
-from time import sleep
 RPL.RoboPiInit("/dev/ttyAMA0", 115200) #connect to RoboPi
 
 a_shoulder = 200
@@ -17,7 +16,8 @@ while True:
             RPL.digitalWrite(1, 1) #turn clockwise
         else:
             RPL.digitalWrite(1, 0) #turn counterclockwise
-        RPL.pwmWrite(0, motor_speed, motor_speed * 2) #RPL.pwmWrite(pin value, number of on pulses, total pulses duration)
-    else:
+        while abs(round(p1, 0) - round(a_shoulder, 0)) > 5:
+            p1 = RPL.analogRead(ppin) * 145 / 512 - 55
+            RPL.pwmWrite(0, motor_speed, motor_speed * 2) #RPL.pwmWrite(pin value, number of on pulses, total pulses duration)
+    if abs(round(p1, 0) - round(a_shoulder, 0)) < 5:
         RPL.pwmWrite(0, 0, 1)
-    sleep(0.1)
