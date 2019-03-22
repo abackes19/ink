7f5ade7e98099e25272372706fa77616fe674837
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2019-03-22 12:07:14 -0500

    Update Pot_motor.py

--- Pot_motor.py
+++ Pot_motor.py
@@ -1,5 +1,4 @@
 import RoboPiLib_pwm as RPL #to pull all the files needed to run the motors
-from time import sleep
 RPL.RoboPiInit("/dev/ttyAMA0", 115200) #connect to RoboPi
 
 a_shoulder = 200
@@ -7,20 +6,19 @@ motor_speed = 500
 
 shoulder_pul = 1
 shoulder_dir = 2
+ppin = 7
 
 print shoulder_pul, " shoulder_pul"
 print shoulder_dir, " shoulder_dir"
+print ppin, " ppin"
 
 RPL.pinMode(shoulder_pul, RPL.PWM) #set shoulder_pul pin as a pulse-width modulation output
 RPL.pinMode(shoulder_dir, RPL.OUTPUT) #set shoulder_dir pin to an output and write 1 to it
 
 RPL.pwmWrite(shoulder_pul, 0, 1)
 
-ppin = 7
-
 while True:
     p1 = RPL.analogRead(ppin) * 145 / 512 - 55
-    print("p = ", int(p1))
     while abs(p1 - a_shoulder) > 5: #so there is a margin of error of max 3 degrees
         if p1 > a_shoulder:
             RPL.digitalWrite(shoulder_dir, 1) #turn clockwise
