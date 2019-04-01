6371416e37f94fa49eb41791a349a12d6b328df6
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2019-04-01 09:57:14 -0500

    Update Pot_motor.py

--- Pot_motor.py
+++ Pot_motor.py
@@ -2,7 +2,6 @@ import RoboPiLib_pwm as RPL #to pull all the files needed to run the motors
 RPL.RoboPiInit("/dev/ttyAMA0", 115200) #connect to RoboPi
 
 a_shoulder = 200
-motor_speed = 500
 
 shoulder_pul = 1
 shoulder_dir = 2
@@ -19,13 +18,14 @@ RPL.pwmWrite(shoulder_pul, 0, 1)
 
 while True:
     p1 = RPL.analogRead(ppin) * 145 / 512 - 55
-    while abs(p1 - a_shoulder) > 5: #so there is a margin of error of max 3 degrees
-        if p1 > a_shoulder:
-            RPL.digitalWrite(shoulder_dir, 1) #turn clockwise
-        if p1 < a_shoulder:
-            RPL.digitalWrite(shoulder_dir, 0) #turn counterclockwise
-        RPL.pwmWrite(shoulder_pul, motor_speed, motor_speed * 2) #RPL.pwmWrite(pin value, number of on pulses, total pulses duration)
-        p1 = RPL.analogRead(ppin) * 145 / 512 - 55
-        if abs(p1 - a_shoulder) < 5:
-            RPL.pwmWrite(shoulder_pul, 0, 1)
-            break
+    print p1, " p1"
+    error = abs(p1 - a_shoulder) #how many degrees off the intended value the arm is
+    if p1 > a_shoulder and error > 5:
+        RPL.digitalWrite(shoulder_dir, 1) #turn clockwise
+        motor_speed = 500
+    if p1 < a_shoulder and error > 5:
+        RPL.digitalWrite(shoulder_dir, 0) #turn counterclockwise
+        motor_speed = 500
+    else:
+        motor_speed = 0 #stop the motor
+    RPL.pwmWrite(shoulder_pul, motor_speed, motor_speed * 2) #RPL.pwmWrite(pin value, number of on pulses, total pulses duration)
