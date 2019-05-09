46e235c1aea372c3f4dd2a331ab19ae82111b2bf
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2019-05-09 08:04:53 -0500

    Update K&P.py

--- K&P.py
+++ K&P.py
@@ -129,7 +129,7 @@ def motor_runner(): #sends signals to all the motors based on potentiometer read
     while quit == False:
         pot_shoulder = RPL.analogRead(ppin_shoulder) * 29 * math.pi / 18432
         error_s = abs(pot_shoulder - a_shoulder) #how many degrees off the intended value the arm is
-        calculated_error_s = error_s / d_one
+        calculated_error_s = error_s * d_one
         print 'calculated error', calculated_error_s
         print 'a_shoulder', a_shoulder
         print 'pot position', pot_shoulder
