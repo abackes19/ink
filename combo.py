4d897eef77e479b329abb0f0c52e6f351b4e1b4a
abackes19 <33259444+abackes19@users.noreply.github.com>
2019-05-29 14:52:21 -0500

    updated d12 and other merging

--- combo.py
+++ combo.py
@@ -31,27 +31,22 @@ clock = pygame.time.Clock()
 step = 4
 originx = 250
 originy = 250
-d_one = 95 # the distance from shoulder to elbow
-d_two = 90 # distance from elbow to wrist
+d_one = 62.0 #distance from shoulder to elbow
+d_two = 48.0 #distance from elbow to wrist
+pixel_one = d_one # the distance from shoulder to elbow
+pixel_two = d_two # distance from elbow to wrist
 
-pygame.draw.circle(gameDisplay, grey, (originx, originy), (d_one + d_two), 0)
-pygame.draw.circle(gameDisplay, white, (originx, originy), (d_one - d_two), 0)
-xm, ym = originx+d_two, originy-d_one
+pygame.draw.circle(gameDisplay, grey, (originx, originy), (pixel_one + pixel_two), 0)
+pygame.draw.circle(gameDisplay, white, (originx, originy), (pixel_one - pixel_two), 0)
+xm, ym = originx+pixel_two, originy-pixel_one
 pygame.draw.line(gameDisplay, blue, (originx, originy), (xm, ym),5)
 pygame.display.update()
 
-x, y = originx+d_two, originy-d_one
+x, y = originx+pixel_two, originy-pixel_one
 xo = x
 yo = y
 x_change = 0
 y_change = 0
-
-s_pin = 1
-e_pin = 0
-input_shoulder = 2400
-input_elbow = 1400
-a_shoulder = math.pi / 2
-a_elbow = math.pi / 2
 
 
 # ^^^ that all would be the setup
@@ -67,13 +62,13 @@ def ik(xm, ym): # here is where we do math
     y = originy - ym
     x = xm - originx
 
-    sqd_one = d_one ** 2
-    sqd_two = d_two ** 2
+    sqd_one = pixel_one ** 2
+    sqd_two = pixel_two ** 2
 
-    d_three = math.sqrt((y**2) + (x**2)) # determining distance from shoulder to wrist ^
-    if d_one - d_two < d_three < d_one + d_two and y > -24:
-        a_three = math.acos((sqd_one + sqd_two - ((y**2) + (x ** 2))) / (2 * d_one * d_two))
-        a_two = math.asin((d_two * math.sin(a_three) / d_three)) # angle between shoulder and wrist
+    pixel_three = math.sqrt((y**2) + (x**2)) # determining distance from shoulder to wrist ^
+    if pixel_one - pixel_two < pixel_three < pixel_one + pixel_two and y > -24:
+        a_three = math.acos((sqd_one + sqd_two - ((y**2) + (x ** 2))) / (2 * pixel_one * pixel_two))
+        a_two = math.asin((pixel_two * math.sin(a_three) / pixel_three)) # angle between shoulder and wrist
         a_four = math.atan2(y , x) # angle between 0 line and wrist
         a_shoulder = (a_four + a_two)  # shoulder angle?
         a_elbow = a_three
@@ -110,38 +105,44 @@ def pos(x, y):
 #display loop
 ######################################
 
-while not done:
-    clock.tick(60)
-    # determine where want to be
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done=True # Flag that we are done so we exit this loop
-        else: # did something other than close
-            x_change, y_change = pos(x,y) # figure out the change
-    # move
-    x += x_change
-    y += y_change
+def display():
+    while not done:
+        clock.tick(60)
+        # determine where want to be
+        for event in pygame.event.get(): # User did something
+            if event.type == pygame.QUIT: # If user clicked close
+                done=True # Flag that we are done so we exit this loop
+            else: # did something other than close
+                x_change, y_change = pos(x,y) # figure out the change
+        # move
+        x += x_change
+        y += y_change
 
-    if ik(x, y) != False:
-        # determine elbow point
-        a_shoulder, a_elbow = ik(x,y)
-        xe = d_one * math.cos(a_shoulder) + originx
-        ye = originy - (d_one * math.sin(a_shoulder))
+        if ik(x, y) != False:
+            # determine elbow point
+            a_shoulder, a_elbow = ik(x,y)
+            xe = pixel_one * math.cos(a_shoulder) + originx
+            ye = originy - (pixel_one * math.sin(a_shoulder))
 
-        xo = x; yo = y
-        # draw line
-        pygame.draw.lines(gameDisplay, blue, False, [[originx,originy], [xe, ye], [xo, yo]], 5)
+            xo = x; yo = y
+            # draw line
+            pygame.draw.lines(gameDisplay, blue, False, [[originx,originy], [xe, ye], [xo, yo]], 5)
 
-    else: # out of range so stay
-        pygame.draw.lines(gameDisplay, pink, False, [[originx,originy], [xe, ye], [xo, yo]], 5)
-        pygame.draw.circle(gameDisplay, pink, (x, y), (5), 0)
+        else: # out of range so stay
+            pygame.draw.lines(gameDisplay, pink, False, [[originx,originy], [xe, ye], [xo, yo]], 5)
+            x = xo; y = yo
+            pygame.draw.circle(gameDisplay, pink, (x, y), (5), 0)
+        
+        global x
+        x = x 
+        global y
+        y = y
 
-
-    pygame.display.update()
-    gameDisplay.fill(grey)
-    pygame.draw.circle(gameDisplay, white, (originx, originy), (d_one + d_two), 0)
-    pygame.draw.circle(gameDisplay, grey, (originx, originy), (d_one - d_two), 0)
-    pygame.draw.rect(gameDisplay, grey, [0, (originy + 24), display_width, display_width])
+        pygame.display.update()
+        gameDisplay.fill(grey)
+        pygame.draw.circle(gameDisplay, white, (originx, originy), (pixel_one + pixel_two), 0)
+        pygame.draw.circle(gameDisplay, grey, (originx, originy), (pixel_one - pixel_two), 0)
+        pygame.draw.rect(gameDisplay, grey, [0, (originy + 24), display_width, display_width])
 
 
 ####
@@ -167,46 +168,12 @@ y = 110.0 #starting y value
 z = 0.0 #starting z value
 speed = 1 #starting speed (whole number between 1 and 4)
 
-microsoft = apple = False #determine which opperating system is being used
-
-print "Press '1' to end code"
 
 ######################################
 # movement functions
 ######################################
 
-def test(): #function for angle domains
-    reach_length = (x ** 2 + y ** 2 + z ** 2) ** 0.5
-    if reach_length > d_one + d_two or reach_length < d_one - d_two:
-        return False
-
-def x_up(): #increase x value
-    global x
-    x += 0.1 * speed
-def x_down(): #decrease x value
-    global x
-    x -= 0.1 * speed
-
-def y_up(): #increase y value
-    global y
-    y += 0.1 * speed
-def y_down(): #decrease y value
-    global y
-    y -= 0.1 * speed
-
-def z_up(): #increase z value
-    global z
-    z += 0.1 * speed
-def z_down(): #decrease z value
-    global z
-    z -= 0.1 * speed
-
-def speed_up(): #increase speed value
-    global speed
-    speed += 1
-def speed_down(): #decrease speed value
-    global speed
-    speed -= 1
+# rip
 
 ######################################
 # setup apple vs mcsft
@@ -231,52 +198,7 @@ except: #if running on microsoft
 # read keys
 ######################################
 
-def key_reader(): #reading input key functions
-    while True:
-        if apple == True:
-            key = sys.stdin.read(1) #reads one character of input without requiring a return command
-        elif microsoft == True:
-            key = msvcrt.getch() #format the keys into readable characters
-
-        if key == '1': #pressing the '1' key kills the process
-            if apple == True:
-                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings) #resets the console settings
-            global quit #to quit out of the motor loop
-            quit = True
-            break
-
-        elif key.upper() == 'X' and speed < 4: #increase speed
-            speed_up()
-        elif key.upper() == 'Z' and speed > 1: #decrease speed
-            speed_down()
-
-        elif key.upper() == 'D': #increase x value
-            x_up()
-            if test() == False:
-                x_down()
-        elif key.upper() == 'A': #decrease x value
-            x_down()
-            if test() == False:
-                x_up()
-
-        elif key.upper() == 'W': #increase y value
-            y_up()
-            if test() == False:
-                y_down()
-        elif key.upper() == 'S': #decrease y value
-            y_down()
-            if test() == False:
-                y_up()
-
-        elif key.upper() == 'E': #increase z value
-            z_up()
-            if test() == False:
-                z_down()
-        elif key.upper() == 'Q': #decrease z value
-            z_down()
-            if test() == False:
-                z_up()
-
+# rip
 
 ######################################
 # motor setup
