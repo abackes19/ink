b9a1cdf62e9839fdf79a073ab10b224d6ed7a39a
abackes19 <33259444+abackes19@users.noreply.github.com>
2019-05-30 12:25:44 -0500

    (ignore last commit) use to test arm freeze w/o pi

--- pygametest4.py
+++ pygametest4.py
@@ -1,57 +1,71 @@
-# using arrow keys! also multiple angles!
+# using arrow keys!
 # notes: can use continuously, but use one key at a time in an orderly fashion
 
-# add angle variable DONE
-# angle change with keys DONE
-# display angle change with keys (calculate like elbow?)
-# topview length change w/ x y change
-
-
-
+import RoboPiLib3 as RPL
+import setup3
 import pygame, math, fractions, time
 from pygame.locals import *
 
+
 pygame.init()
 
+######################################
+#setup
+######################################
 
-white = (255,255,255); black = (0,0,0)
-red = (255,0,0); green = (39, 147, 52)
-blue = (102, 136, 214); pink = (232, 13, 119)
+
+white = (255,255,255)
+black = (0,0,0)
+red = (255,0,0)
+green = (39, 147, 52)
+blue = (102, 136, 214)
+pink = (232, 13, 119)
 grey = (203, 206, 214)
 
-display_width = 1000
-display_height = 450
-screen = pygame.display.set_mode((display_width,display_height))
-screen.fill(white)
+display_width = 500
+display_height = 500
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+gameDisplay.fill(white)
 clock = pygame.time.Clock()
 
-step = 2
+step = 4
 originx = 250
 originy = 250
-d_one = 124 # the distance from shoulder to elbow
-d_two = 96 # distance from elbow to wrist
-toriginx = 725
-toriginz = 250
+d_one = 95 # the distance from shoulder to elbow
+d_two = 90 # distance from elbow to wrist
 
+pygame.draw.circle(gameDisplay, grey, (originx, originy), (d_one + d_two), 0)
+pygame.draw.circle(gameDisplay, white, (originx, originy), (d_one - d_two), 0)
 xm, ym = originx+d_two, originy-d_one
+pygame.draw.line(gameDisplay, blue, (originx, originy), (xm, ym),5)
+pygame.display.update()
+
 x, y = originx+d_two, originy-d_one
-z = 0
 xo = x
 yo = y
 x_change = 0
 y_change = 0
-z_change = 0
-td_one = d_one + d_two
 
-done = False
+s_pin = 1
+e_pin = 0
+input_shoulder = 2400
+input_elbow = 1400
+a_shoulder = math.pi / 2
+a_elbow = math.pi / 2
+
+
 # ^^^ that all would be the setup
 
+done = False
+clock = pygame.time.Clock()
 
+######################################
+#display functions
+######################################
 
 def ik(xm, ym): # here is where we do math
     y = originy - ym
     x = xm - originx
-
 
     sqd_one = d_one ** 2
     sqd_two = d_two ** 2
@@ -63,25 +77,21 @@ def ik(xm, ym): # here is where we do math
         a_four = math.atan2(y , x) # angle between 0 line and wrist
         a_shoulder = (a_four + a_two)  # shoulder angle?
         a_elbow = a_three
-        a_swivel = math.atan2(round(x, 2), round(z, 2))
 
-        xe = d_one * math.cos(a_shoulder) + originx
-        ye = originy - (d_one * math.sin(a_shoulder))
-        ze = toriginz - (xe * math.tan(a_swivel))
-        return xe, ye, ze
+        return a_shoulder, a_elbow
     else:
         return False
 
     pygame.display.flip()
 
-def pos(x, y, z):
+
+def pos(x, y):
     x_change = 0
     y_change = 0
-    z_change = 0
 
     if event.type == pygame.KEYDOWN:
         # what key are they pressing? move accordingly
-        if event.key == pygame.K_ESCAPE:
+        if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
             done=True
             return done
         elif event.key == pygame.K_a:
@@ -92,12 +102,13 @@ def pos(x, y, z):
             y_change = -step
         elif event.key == pygame.K_s:
             y_change = step
-        elif event.key == pygame.K_q:
-            z_change = -step
-        elif event.key == pygame.K_e:
-            z_change = step
 
-    return x_change, y_change, z_change
+    return x_change, y_change
+
+
+######################################
+#display loop
+######################################
 
 while not done:
     clock.tick(60)
@@ -106,35 +117,33 @@ while not done:
         if event.type == pygame.QUIT: # If user clicked close
             done=True # Flag that we are done so we exit this loop
         else: # did something other than close
-            x_change, y_change, z_change = pos(x,y,z) # figure out the change
-
+            x_change, y_change = pos(x,y) # figure out the change
     # move
-    x += x_change; y += y_change; z += z_change
+    x += x_change
+    y += y_change
 
     if ik(x, y) != False:
         # determine elbow point
-        xe, ye = ik(x,y)
+        a_shoulder, a_elbow = ik(x,y)
+        xe = d_one * math.cos(a_shoulder) + originx
+        ye = originy - (d_one * math.sin(a_shoulder))
 
-        xo = x; yo = y; zo = z
+        xo = x; yo = y
         # draw line
-        pygame.draw.lines(screen, blue, False, [[originx,originy], [xe, ye], [xo, yo]], 5) # sideview
-#        pygame.draw.line(screen, blue, [toriginx,toriginy], [toriginx + d_one, toriginy + d_two], 5) # not sure what this was
-        pygame.draw.line(screen, blue, [toriginx, toriginy], [xo + toriginx, toriginz - z])
-        pygame.draw.line(screen, green, [toriginx, toriginy], [xe - originx + toriginx, ze])
+        pygame.draw.lines(gameDisplay, blue, False, [[originx,originy], [xe, ye], [xo, yo]], 5)
 
     else: # out of range so stay
-        pygame.draw.lines(screen, pink, False, [[originx,originy], [xe, ye], [xo, yo]], 5)
-        pygame.draw.circle(screen, pink, (x, y), (5), 0)
+        x = xo, y = yo
+        pygame.draw.lines(gameDisplay, pink, False, [[originx,originy], [xe, ye], [xo, yo]], 5)
+        pygame.draw.circle(gameDisplay, pink, (x, y), (5), 0)
 
-# Be IDLE friendly
+
     pygame.display.update()
-    screen.fill(grey)
-    pygame.draw.circle(screen, white, (originx, originy), (d_one + d_two), 0)
-    pygame.draw.circle(screen, grey, (originx, originy), (d_one - d_two), 0)
-    pygame.draw.rect(screen, grey, [0, (originy + 24), display_width, display_width])
-    # topview
-    pygame.draw.circle(screen, white, (toriginx, toriginy), (d_one + d_two), 0)
-    pygame.draw.circle(screen, grey, (toriginx, toriginy), (d_one - d_two), 0)
+    gameDisplay.fill(grey)
+    pygame.draw.circle(gameDisplay, white, (originx, originy), (d_one + d_two), 0)
+    pygame.draw.circle(gameDisplay, grey, (originx, originy), (d_one - d_two), 0)
+    pygame.draw.rect(gameDisplay, grey, [0, (originy + 24), display_width, display_width])
 
-#please work rectangle
+
+####
 pygame.quit()
