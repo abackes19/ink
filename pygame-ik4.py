8351aaec7cf4a22e83f676da2e741e5011703bda
abackes19 <33259444+abackes19@users.noreply.github.com>
2019-05-31 11:40:49 -0500

    Update pygame-ik4.py

--- pygame-ik4.py
+++ pygame-ik4.py
@@ -122,7 +122,7 @@ while not done:
 
 #        pygame.draw.line(screen, blue, [toriginx,toriginz], [toriginx + d_one, toriginz + d_two], 5) # not sure what this was
         pygame.draw.line(screen, blue, (toriginz, toriginx), [(toriginz - z), (pxo)], 5)
-        pygame.draw.line(screen, green, (toriginz, toriginx), (ze, pxe), 5)
+#        pygame.draw.line(screen, green, (toriginz, toriginx), (ze, pxe), 5)
     else: # out of range so stay
         pygame.draw.lines(screen, pink, False, [[originx,originy], [xe, ye], [xo, yo]], 5)
         pygame.draw.circle(screen, pink, (x, y), (5), 0)
