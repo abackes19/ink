85bc61ce1a2192f49302bd6072adc0fef7583f12
cweatherly19 <33259443+cweatherly19@users.noreply.github.com>
2019-06-04 09:23:32 -0500

    Update Pot.py

--- Pot.py
+++ Pot.py
@@ -1,5 +1,5 @@
-import RoboPiLib as RPL
-import setup
+import RoboPiLib_pwm as RPL
+RPL.RoboPiInit("/dev/ttyAMA0", 115200)
 from time import sleep
 
 apin = 0
@@ -7,7 +7,5 @@ ppin = 7  #potentiomenter pin line
 
 while True:
   p1 = RPL.analogRead(ppin)
-  a1 = p1 * 145 / 512
-  RPL.servoWrite(apin,int(a1))
-  print("p = ", int(p1), "  a = ",int(a1 - 55) )
+  print("p = ", int(pl))
   sleep(0.05)
