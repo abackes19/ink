0cc8b9ec34f91b6b8b23bb037104d92fd7767f78
loaner <loaner@bsmzcf5yvair.bsm-online.local>
2019-06-10 14:55:27 -0500

    trying to send variable

--- testsocketc.py
+++ testsocketc.py
@@ -4,12 +4,15 @@ import socket
 # Create a socket object
 s = socket.socket()
 
+x = 87
+
+
 # Define the port on which you want to connect
 port = 12345
 
 # connect to the server on local computer
 s.connect(('192.168.21.135', port))
-s.sendall("Big oof")
+s.sendall(x)
 
 
 # receive data from the server
