c45e42fa64750ebbca2f516345210f9bb685315c
loaner <loaner@bsmzcf5yvair.bsm-online.local>
2019-06-10 14:37:41 -0500

    trying another socket method

--- testsocket.py
+++ testsocket.py
@@ -1,28 +1,36 @@
-# this works! next step: do we send a file or can we just send variables?
+# first of all import the socket library
 import socket
-import time
 
-# create a socket object
-serversocket = socket.socket(
-	        socket.AF_INET, socket.SOCK_STREAM)
+# next create a socket object
+s = socket.socket()
+print "Socket successfully created"
 
-# get local machine name
-host = socket.gethostname()
-print host
+# reserve a port on your computer in our
+# case it is 12345 but it can be anything
+port = 12345
 
-port = 9999
+# Next bind to the port
+# we have not typed any ip in the ip field
+# instead we have inputted an empty string
+# this makes the server listen to requests
+# coming from other computers on the network 
+s.bind(('', port))
+print "socket binded to %s" %(port)
 
-# bind to the port
-serversocket.bind((host, port))
+# put the socket into listening mode
+s.listen(5)
+print "socket is listening"
 
-# queue up to 5 requests
-serversocket.listen(5)
-
+# a forever loop until we interrupt it or
+# an error occurs
 while True:
-    # establish a connection
-    clientsocket,addr = serversocket.accept()
 
-    print("Got a connection from %s" % str(addr))
-    currentTime = time.ctime(time.time()) + "\r\n"
-    clientsocket.send(currentTime.encode('ascii'))
-    clientsocket.close()
+   # Establish connection with client.
+   c, addr = s.accept()
+   print 'Got connection from', addr
+
+   # send a thank you message to the client.
+   c.send('Thank you for connecting')
+
+   # Close the connection with the client
+   c.close()
--- testsocketc.py
+++ testsocketc.py
@@ -1,20 +1,16 @@
-2# client.py
+# Import socket module
 import socket
 
-# create a socket object
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Create a socket object
+s = socket.socket()
+  
+# Define the port on which you want to connect
+port = 12345
 
-# get local machine name
-host = socket.gethostname()
+# connect to the server on local computer
+s.connect(('127.0.0.1', port))
 
-port = 9999
-print host
-# connection to hostname on the port.
-s.connect((host, port))
-
-# Receive no more than 1024 bytes
-tm = s.recv(1024)
-
+# receive data from the server
+print s.recv(1024)
+# close the connection
 s.close()
-
-print("The time got from the server is %s" % tm.decode('ascii'))
